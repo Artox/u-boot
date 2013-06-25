6e381a1530adefcdf19890b754e871f1958b1608
Sascha Silbe <t-uboot@infra-silbe.de>
2013-06-25 22:45:17 +0200

    block: mvsata: add dove include
    Dove SoC also uses mvsata, therefore add a SoC specific include to
    allow to reuse the mvsata ide driver.
    
    Signed-off-by: Sascha Silbe <t-uboot@infra-silbe.de>
    Acked-by: Prafulla Wadaskar <prafulla@marvell.com>

--- drivers/block/mvsata_ide.c
+++ drivers/block/mvsata_ide.c
@@ -13,6 +13,8 @@
 #include <asm/arch/orion5x.h>
 #elif defined(CONFIG_KIRKWOOD)
 #include <asm/arch/kirkwood.h>
+#elif defined(CONFIG_DOVE)
+#include <asm/arch/dove.h>
 #endif
 
 /* SATA port registers */
